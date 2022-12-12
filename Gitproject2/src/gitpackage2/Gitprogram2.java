24ae665e86cdb228070e1aa22324fe20bfecb789
soumya24-1 <mbsoumya24@gmail.com>
2022-12-12 10:51:25 +0530

    commitinggg 2

--- Gitproject2/src/gitpackage2/Gitprogram2.java
+++ Gitproject2/src/gitpackage2/Gitprogram2.java
@@ -5,6 +5,7 @@ public class Gitprogram2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi iiiiii");
+		System.out.println("helloooooo vvagg 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");
 
 	}
 
