099829098cc2d0bd26bbdffe074294f52e673be5
soumya24-1 <mbsoumya24@gmail.com>
2022-12-12 10:42:07 +0530

    commit 1

--- Gitproject2/src/gitpackage2/Gitprogram2.java
+++ Gitproject2/src/gitpackage2/Gitprogram2.java
@@ -4,7 +4,7 @@ public class Gitprogram2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hiiiiiii");
+		System.out.println("hi iiiiii");
 
 	}
 
